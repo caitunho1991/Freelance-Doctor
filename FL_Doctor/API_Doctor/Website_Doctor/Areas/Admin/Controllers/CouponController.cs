65e31b1dab0c4576bd939a96c5108e6d4e2d2f91
caitunho1991 <victornguyen305@yahoo.com>
2018-12-20 13:35:27 +0700

    Update Coupon Web Admin

--- FL_Doctor/API_Doctor/Website_Doctor/Areas/Admin/Controllers/CouponController.cs
+++ FL_Doctor/API_Doctor/Website_Doctor/Areas/Admin/Controllers/CouponController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Website_Doctor.Areas.Admin.Data;
+using Website_Doctor.Areas.Admin.Models;
 
 namespace Website_Doctor.Areas.Admin.Controllers
 {
@@ -11,8 +13,74 @@ namespace Website_Doctor.Areas.Admin.Controllers
         // GET: Admin/Coupon
         public ActionResult Index()
         {
-            //var coupons = _context.Coupons.Where(x=>x.);
-            return View();
+            if (this.CheckAuth() == false)
+            {
+                return RedirectToAction("Login", "Accounts");
+            }
+            ViewBag.Title = "Danh sách mã giảm giá";
+            var coupon = _context.Coupons.OrderByDescending(x => x.DateCreate);
+            return View("Index", coupon);
+        }
+
+        public ActionResult CreateOrEdit(int? ID)
+        {
+            if (this.CheckAuth() == false)
+            {
+                return RedirectToAction("Login", "Accounts");
+            }
+            if (ID != null && ID > 0)
+            {
+                var c = _context.Coupons.SingleOrDefault(x => x.ID == ID);
+                VM_Coupons prod = new VM_Coupons();
+                prod = prod.ConvertDataToModel(c);
+                return View("CreateOrEdit", prod);
+            }
+            return View("CreateOrEdit");
+        }
+        [HttpPost]
+        public ActionResult CreateOrEdit(VM_Coupons p)
+        {
+            if (this.CheckAuth() == false)
+            {
+                return RedirectToAction("Login", "Accounts");
+            }
+            if (!ModelState.IsValid)
+            {
+                return View("CreateOrEdit", p);
+            }
+            if (p.ID != null && p.ID > 0)
+            {
+                //edit
+                Coupon prod = _context.Coupons.Find(p.ID);
+                prod.DateStart = p.StartDate;
+                prod.DateEnd = p.EndDate;
+                if (p.CouponType == "true")
+                {
+                    prod.Value = p.Value;
+                    prod.Percent = null;
+                }
+                else
+                {
+                    prod.Percent = p.Percent;
+                    prod.Value = null;
+                }
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                //create
+                if (_context.Products.Any(x => x.code.Equals(p.Code)))
+                {
+                    TempData["Err"] = "Chuyên khoa khám bệnh đã tồn tại trong hệ thống";
+                    return View("CreateOrEdit", p);
+                }
+                var prod = p.ConvertModelToData();
+                _context.Coupons.Add(prod);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
         }
     }
 }
---
+++ FL_Doctor/API_Doctor/Website_Doctor/Areas/Admin/Models/VM_Coupons.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using Website_Doctor.Areas.Admin.Data;
+
+namespace Website_Doctor.Areas.Admin.Models
+{
+    public class VM_Coupons
+    {
+        public int? ID { get; set; }
+        [Required(ErrorMessage = "Vui lòng nhập tên mã giảm giá")]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        [Required(ErrorMessage = "Vui lòng nhập mã giảm giá")]
+        public string Code { get; set; }
+        [Required(ErrorMessage = "Vui lòng nhập số lượt sử dụng")]
+        public int Count { get; set; }
+        [Required(ErrorMessage = "Vui lòng nhập ngày bắt đầu")]
+        public DateTime StartDate { get; set; }
+        [Required(ErrorMessage = "Vui lòng nhập ngày kết thúc")]
+        public DateTime EndDate { get; set; }
+        public Decimal Value { get; set; }
+        public Decimal Percent { get; set; }
+        public string CouponType { get; set; }
+
+        public Coupon ConvertModelToData()
+        {
+            Coupon prod = new Coupon();
+            prod.Name = this.Name;
+            prod.Description = this.Description;
+            prod.Code = this.Code;
+            prod.Count = this.Count;
+            prod.DateStart = this.StartDate;
+            prod.DateEnd = this.EndDate;
+            prod.DateCreate = DateTime.Now;
+            if (this.CouponType == "true")
+            {
+                prod.Value = this.Value;
+            }
+            else
+            {
+                prod.Percent = this.Percent;
+            }
+            return prod;
+        }
+
+        public VM_Coupons ConvertDataToModel(Coupon p)
+        {
+            VM_Coupons prod = new VM_Coupons();
+            prod.ID = p.ID;
+            prod.Name = p.Name;
+            prod.Description = p.Description;
+            prod.Code = p.Code;
+            prod.Count = (int)p.Count;
+            prod.StartDate = (DateTime)p.DateStart;
+            prod.EndDate = (DateTime)p.DateEnd;
+            prod.Value = p.Value != null ? (Decimal)p.Value : -1;
+            prod.Percent = p.Percent != null ? (Decimal)p.Percent : -1;
+            return prod;
+        }
+    }
+}
